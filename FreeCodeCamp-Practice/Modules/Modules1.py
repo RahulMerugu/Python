1e9c835dbeb79d19fd0f69a23926f7b20617abd7
RahulMerugu <rahulmofficial0105@gmail.com>
2025-07-27 20:27:24 +0530

    Delete FreeCodeCamp-Practice directory

--- FreeCodeCamp-Practice/Modules/Modules1.py
+++
@@ -1,5 +0,0 @@
-def dog():  
-    print ('Woof')
-
-
-#This is done to show modularity, and that a single program can be broken down into multiple files for compartmentalization and convienience and is easier to fix.
